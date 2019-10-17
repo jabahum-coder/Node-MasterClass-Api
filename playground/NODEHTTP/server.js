f926ba4e04cd76441ac87b0647ba52c88c123dea
Samuel <js.rosemond@gmail.com>
2019-10-17 00:48:34 -0400

    HTTP quick overview

--- playground/NODEHTTP/server.js
+++ playground/NODEHTTP/server.js
@@ -6,18 +6,55 @@ const PORT = 5000;
 const todos = [ { id: 1, text: 'todo one' }, { id: 2, text: 'todo two' }, { id: 3, text: 'todo three' } ];
 
 const server = http.createServer(function(req, res) {
-	res.statusCode = 201;
-	res.setHeader('Content-Type', 'application/json');
-	res.setHeader('X-Powered-By', 'Node.js');
-	// Better -v below
-	res.writeHead();
+	// res.statusCode = 201;
+	// res.setHeader('Content-Type', 'application/json');
+	// res.setHeader('X-Powered-By', 'Node.js');
 
-	res.end(
-		JSON.stringify({
-			success: true,
-			data: todos
+	// Better -v below res.writeHead(status,data)
+
+	// Send custom body data
+	const { method, url } = req;
+	let body = [];
+
+	req
+		.on('data', (chunk) => {
+			body.push(chunk);
 		})
-	);
+		.on('end', () => {
+			body = Buffer.concat(body).toString();
+
+			let status = 404;
+			const response = {
+				success: false,
+				data: null,
+				error: null
+			};
+
+			if (method === 'GET' && url === '/todos') {
+				status = 200;
+				response.success = true;
+				response.data = todos;
+			} else if (method === 'POST' && url === '/todos') {
+				const { id, text } = JSON.parse(body);
+
+				if (!id || !text) {
+					status = 400;
+					response.error = 'Please add id and text';
+				} else {
+					todos.push({ id, text });
+					status = 201;
+					response.success = true;
+					response.data = todos;
+				}
+			}
+
+			res.writeHead(status, {
+				'Content-Type': 'application/json',
+				'X-Powered-By': 'Node.js'
+			});
+
+			res.end(JSON.stringify(response));
+		});
 });
 
 server.listen(PORT, () => {
