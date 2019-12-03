ab295a27b5ca38dd86a80a033b3ee86380e97b26
Samuel <js.rosemond@gmail.com>
2019-12-03 10:00:12 -0500

    complete -documentation

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const path = require('path');
 const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const rateLimit =  require('express-rate-limit')
+const hpp = require('hpp')
+const cors = require('cors')
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
@@ -42,6 +47,24 @@ app.use(fileUpload());
 
 // Sanitize data
 app.use(mongoSanitize())
+
+//Set security headers
+app.use(helmet())
+
+//Prevent XSS attacks
+app.use(xss())
+
+//Rate limiting
+const limiter = rateLimit({
+	windowMs: 10* 60 * 1000,
+	max:100
+})
+
+//Prevent http param pollution
+app.use(hpp())
+
+// Enable CORS
+app.use(cors())
 
 // Set static folder
 /* note to self change later to
